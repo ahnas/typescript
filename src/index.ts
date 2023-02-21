eae4e29ed389469857524d9cebfabd8a41c7993d
ahnas <muhammed.ahnas@bt.com>
2023-02-21 14:44:45 +0530

    datatype automatically find

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-let age: number =20;
-if (age<50)
-    age += 10;
-console.log(age);
+let age=20;
+let course ="Typescript";
+let is_pubisshed= true;
+let set;
+
+// in type script no need to annotate the type.by assigning the value it will automatically find the datatype.
